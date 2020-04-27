40d4a6f67921ce7c7df29c07b963dbfa5fc4bcac
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-27 15:51:57 +0200

    doing more cleanup editing startup scripts writing feature test 'User can calculate their BMI' adding form to html page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,26 @@ import React, { Component } from 'react'
  class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <form>
+        <label htmlFor="weight">Weight</label>
+        <input
+          type="number"
+          required
+          placeholder="Weight in kgs"
+          name="weight"
+          id="weight"
+        />
+        <label htmlFor="height">Height</label>
+        <input
+          type="number"
+          required
+          placeholder="Height in cm"
+          name="height"
+          id="height"
+        />
+        <button id="calculate">Calculate BMI</button>
+      </form>
+    );
   }
 }
 export default App;
